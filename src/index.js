9bf258ba640b89722e94b18ef608e7fcdaf3c602
curtis_zhan <>
2021-11-10 23:42:48 +0800

    create project base

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/scss/reset.scss';
+import './assets/scss/variables.scss';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
